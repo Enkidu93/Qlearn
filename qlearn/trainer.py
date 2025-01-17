a02843e55d694df26ea05606172e01ecd938ac42
Enkidu93 <lowryec17@gcc.edu>
2025-01-17 17:38:17 -0500

    Format files

--- qlearn/trainer.py
+++ qlearn/trainer.py
@@ -2,25 +2,38 @@ from typing import Callable, Optional
 from .data_models import Environment, QAgent
 from tqdm import tqdm
 
+
 class QLearnTrainer:
     def __init__(self, environment: Environment, agent: QAgent):
         self.environment = environment
         self.agent = agent
 
-    def train(self, num_episodes: Optional[int] = None, num_actions: Optional[int] = None, get_replay: Callable[[QAgent, Environment],None] = None, get_replay_interval:Optional[int] = None, load_preexisting_agent: bool = False) -> tuple[list[float], list[int]]:
-        assert bool(num_episodes)^bool(num_actions), "Exactly one of episodes and num_actions must be set"
+    def train(
+        self,
+        num_episodes: Optional[int] = None,
+        num_actions: Optional[int] = None,
+        get_replay: Callable[[QAgent, Environment], None] = None,
+        get_replay_interval: Optional[int] = None,
+        load_preexisting_agent: bool = False,
+    ) -> tuple[list[float], list[int]]:
+        assert bool(num_episodes) ^ bool(
+            num_actions
+        ), "Exactly one of episodes and num_actions must be set"
         rewards = []
         episode_lengths = []
         if num_actions:
             agent_actions = 0
-            while True: #TODO refactor to a single loop; move logic to methods
+            while True:  # TODO refactor to a single loop; move logic to methods
                 self.environment.reset(self.agent)
                 if self.agent.action_number > 0 or load_preexisting_agent:
-                        self.agent.load()
+                    self.agent.load()
                 while not self.environment.complete:
                     episode_length = 0
                     if self.agent.action_number > 0:
-                        if get_replay_interval is not None and (i % get_replay_interval) == 0:
+                        if (
+                            get_replay_interval is not None
+                            and (i % get_replay_interval) == 0
+                        ):
                             get_replay(self.agent, self.environment)
                     self.agent.take_action()
                     agent_actions += 1
@@ -40,19 +53,22 @@ class QLearnTrainer:
                 self.environment.reset(self.agent)
                 episode_length = 0
                 if i > 0 or load_preexisting_agent:
-                        self.agent.load()
+                    self.agent.load()
                 while not self.environment.complete:
                     if self.agent.action_number > 0:
-                        if get_replay_interval is not None and (i % get_replay_interval) == 0:
+                        if (
+                            get_replay_interval is not None
+                            and (i % get_replay_interval) == 0
+                        ):
                             get_replay(self.agent, self.environment)
                     self.agent.take_action()
                     episode_length += 1
                 self.agent.save()
                 rewards.append(self.agent.total_r)
                 episode_lengths.append(episode_length)
-        
+
         return rewards, episode_lengths
-    
+
     def test(self) -> tuple[float, int]:
         self.agent.load()
         self.environment.reset(self.agent)
@@ -60,8 +76,5 @@ class QLearnTrainer:
         while not self.environment.complete:
             self.agent.take_action()
             episode_length += 1
-        
+
         return self.agent.total_r, episode_length
-
-
-  
--- scratch.py
+++ scratch.py
@@ -7,47 +7,50 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from qlearn.trainer import QLearnTrainer
 
-environment = ChaseGridWorld(25)
-agent = QAgent(environment,name="TestAgentChase25", epsilon=1.0, alpha=1.0, epsilon_decay=lambda x: x*0.99994, alpha_decay=lambda x: x*0.999975)
+environment = ChaseGridWorld(40)
+agent = QAgent(environment,name="TestAgentChase25", epsilon=0.0, alpha=0.0, epsilon_decay=lambda x: x*0.99994, alpha_decay=lambda x: x*0.999975)
 trainer = QLearnTrainer(environment, agent)
 
-REPLAY_INTERVAL=5_000
-replays = []
-prev_episode_number = -1
-def get_replay(agent:QAgent, environment:Environment):
-    global prev_episode_number
-    if agent.episode_number > prev_episode_number:
-        replays.append([])
-        prev_episode_number = agent.episode_number
-    if 'target_state' in environment.__dict__:
-        replays[-1].append((agent.state.qualities[0:2], environment.target_state.qualities))
-    else:
-        replays[-1].append(tuple([agent.state.qualities[0:2]]))
+agent.load()
+print(trainer.test())
 
-rewards, game_lengths = trainer.train(num_episodes=80_000, get_replay=get_replay, get_replay_interval=REPLAY_INTERVAL)
-plt.plot(rewards)
-plt.show()
-plt.plot(game_lengths)
-plt.show()
+# REPLAY_INTERVAL=5_000
+# replays = []
+# prev_episode_number = -1
+# def get_replay(agent:QAgent, environment:Environment):
+#     global prev_episode_number
+#     if agent.episode_number > prev_episode_number:
+#         replays.append([])
+#         prev_episode_number = agent.episode_number
+#     if 'target_state' in environment.__dict__:
+#         replays[-1].append((agent.state.qualities[0:2], environment.target_state.qualities))
+#     else:
+#         replays[-1].append(tuple([agent.state.qualities[0:2]]))
 
-for i, replay in enumerate(replays):
-    fig, ax = plt.subplots()
-    ax.set_title(f"Episode = {(i+1)*REPLAY_INTERVAL}")
-    circle = plt.Circle((environment.starting_state.qualities[0], environment.starting_state.qualities[1]),0.5*(environment.size/25),fc='r')
-    if 'target_state' in environment.__dict__:
-        target = plt.Circle((environment.target_starting_state.qualities[0], environment.target_starting_state.qualities[1]),0.5*(environment.size/25),fc='b')
-        ax.add_artist(target)
-    ax.set_xlim(0,environment.size)
-    ax.set_ylim(0,environment.size)
-    ax.set_autoscale_on(True)
-    ax.add_artist(circle)
+# rewards, game_lengths = trainer.train(num_episodes=80_000, get_replay=get_replay, get_replay_interval=REPLAY_INTERVAL)
+# plt.plot(rewards)
+# plt.show()
+# plt.plot(game_lengths)
+# plt.show()
 
-    def animate(ix):
-        circle.center = replay[ix][0]
-        if 'target_state' in environment.__dict__:
-            target.center = replay[ix][1]
-        return circle,
+# for i, replay in enumerate(replays):
+#     fig, ax = plt.subplots()
+#     ax.set_title(f"Episode = {(i+1)*REPLAY_INTERVAL}")
+#     circle = plt.Circle((environment.starting_state.qualities[0], environment.starting_state.qualities[1]),0.5*(environment.size/25),fc='r')
+#     if 'target_state' in environment.__dict__:
+#         target = plt.Circle((environment.target_starting_state.qualities[0], environment.target_starting_state.qualities[1]),0.5*(environment.size/25),fc='b')
+#         ax.add_artist(target)
+#     ax.set_xlim(0,environment.size)
+#     ax.set_ylim(0,environment.size)
+#     ax.set_autoscale_on(True)
+#     ax.add_artist(circle)
 
-    animation = FuncAnimation(fig, animate, frames=len(replay), interval=75)
-    plt.grid(linewidth=3.0, alpha=0.5, color='grey')
-    plt.show()
+#     def animate(ix):
+#         circle.center = replay[ix][0]
+#         if 'target_state' in environment.__dict__:
+#             target.center = replay[ix][1]
+#         return circle,
+
+#     animation = FuncAnimation(fig, animate, frames=len(replay), interval=75)
+#     plt.grid(linewidth=3.0, alpha=0.5, color='grey')
+#     plt.show()
