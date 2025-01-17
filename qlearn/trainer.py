85374a9c31cb76be9b567e8c76dc200ded10ff6c
Enkidu93 <lowryec17@gcc.edu>
2025-01-17 08:44:10 -0500

    Rename var

--- qlearn/trainer.py
+++ qlearn/trainer.py
@@ -10,7 +10,7 @@ class QLearnTrainer:
     def train(self, num_episodes: Optional[int] = None, num_actions: Optional[int] = None, get_replay: Callable[[QAgent, Environment],None] = None, get_replay_interval:Optional[int] = None, load_preexisting_agent: bool = False) -> tuple[list[float], list[int]]:
         assert bool(num_episodes)^bool(num_actions), "Exactly one of episodes and num_actions must be set"
         rewards = []
-        game_lengths = []
+        episode_lengths = []
         if num_actions:
             agent_actions = 0
             while True: #TODO refactor to a single loop; move logic to methods
@@ -18,19 +18,19 @@ class QLearnTrainer:
                 if self.agent.action_number > 0 or load_preexisting_agent:
                         self.agent.load()
                 while not self.environment.complete:
-                    game_length = 0
+                    episode_length = 0
                     if self.agent.action_number > 0:
                         if get_replay_interval is not None and (i % get_replay_interval) == 0:
                             get_replay(self.agent, self.environment)
                     self.agent.take_action()
                     agent_actions += 1
-                    game_length += 1
+                    episode_length += 1
                     if agent_actions >= num_actions:
                         break
 
                 self.agent.save()
                 rewards.append(self.agent.total_r)
-                game_lengths.append(game_length)
+                episode_lengths.append(episode_length)
 
                 if agent_actions >= num_actions:
                     break
@@ -38,7 +38,7 @@ class QLearnTrainer:
         if num_episodes:
             for i in tqdm(range(num_episodes)):
                 self.environment.reset(self.agent)
-                game_length = 0
+                episode_length = 0
                 if i > 0 or load_preexisting_agent:
                         self.agent.load()
                 while not self.environment.complete:
@@ -46,22 +46,22 @@ class QLearnTrainer:
                         if get_replay_interval is not None and (i % get_replay_interval) == 0:
                             get_replay(self.agent, self.environment)
                     self.agent.take_action()
-                    game_length += 1
+                    episode_length += 1
                 self.agent.save()
                 rewards.append(self.agent.total_r)
-                game_lengths.append(game_length)
+                episode_lengths.append(episode_length)
         
-        return rewards, game_lengths
+        return rewards, episode_lengths
     
     def test(self) -> tuple[float, int]:
         self.agent.load()
         self.environment.reset(self.agent)
-        game_length = 0
+        episode_length = 0
         while not self.environment.complete:
             self.agent.take_action()
-            game_length += 1
+            episode_length += 1
         
-        return self.agent.total_r, game_length
+        return self.agent.total_r, episode_length
 
 
   
